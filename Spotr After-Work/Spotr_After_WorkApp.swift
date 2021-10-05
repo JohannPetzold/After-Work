afeb8b551c9b0cf6f4d08dadd09c56d399dbbf7b
Johann Petzold <johann.petzold@gmail.com>
2021-10-05 10:26:24 +0200

    Initial Commit

---
+++ Spotr After-Work/Spotr_After_WorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotr_After_WorkApp.swift
+//  Spotr After-Work
+//
+//  Created by Johann Petzold on 05/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Spotr_After_WorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
